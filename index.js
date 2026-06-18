2d7596544accfa82552569b42fe55cab78ce8779
biingus777 <ks9cksbytb@privaterelay.appleid.com>
2026-06-18 09:00:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// 1. Importy (require, const client = new Client(), itp.)
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
 
-// 2. TUTAJ DODAJ KOD DO CZYSZCZENIA KOMEND
 if (process.env.CLEAR_COMMANDS === 'true') {
-  client.once('ready', async () => {
+  const tempClient = new Client({ intents: [GatewayIntentBits.Guilds] });
+  tempClient.once('ready', async () => {
     try {
-      await client.application.commands.set([]);
+      await tempClient.application.commands.set([]);
       console.log('✅ Komendy wyczyszczone!');
       process.exit(0);
     } catch (error) {
@@ -14,49 +15,43 @@ if (process.env.CLEAR_COMMANDS === 'true') {
       process.exit(1);
     }
   });
-}
+  tempClient.login(process.env.DISCORD_TOKEN);
+} else {
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.DirectMessages,
+    ],
+  });
 
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
+  client.commands = new Collection();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.DirectMessages,
-  ],
-});
-
-client.commands = new Collection();
-
-// Ładowanie komend
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if (command.data && command.execute) {
-    client.commands.set(command.data.name, command);
+  // Ładowanie komend
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if (command.data && command.execute) {
+      client.commands.set(command.data.name, command);
+    }
   }
-}
 
-// Ładowanie eventów
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
+  // Ładowanie eventów
+  const eventsPath = path.join(__dirname, 'events');
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
   }
-}
 
-client.login(process.env.DISCORD_TOKEN);
+  client.login(process.env.DISCORD_TOKEN);
+}
